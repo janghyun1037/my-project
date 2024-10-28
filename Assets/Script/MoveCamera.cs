ddfbd2fc471e7e7950a8a1c310a66a8aecc6e881
janghyun1037 <hyunjhw@naver.com>
2024-10-28 23:15:44 +0900

    camera move
    카메라 움직임 제어

--- Assets/Script/MoveCamera.cs
+++ Assets/Script/MoveCamera.cs
@@ -6,9 +6,22 @@ public class MoveCamera : MonoBehaviour
 {
     public Transform target;
     public float speed;
+
+    public Vector2 center;
+    public Vector2 size;
+    float height;
+    float width;
+
     void Start()
     {
-        
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, size);
     }
 
     void LateUpdate()
@@ -16,5 +29,13 @@ public class MoveCamera : MonoBehaviour
         //transform.position = new Vector3(target.position.x, target.position.y + 3, -10f);     // 타겟 위치의 x값, 타겟 위치의 y값 이떄 y를 올릴려면 +를 하면됨
         transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
         transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+
+        float lx = size.x * 0.5f - width;
+        float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
+
+        float ly = size.y * 0.5f - height;
+        float clampY = Mathf.Clamp(transform.position.y, -ly + center.x, ly + center.y);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
     }
 }
