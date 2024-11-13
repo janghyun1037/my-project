16b2e1df006aa80384528d4f3d646f38bde02ac3
janghyun1037 <hyunjhw@naver.com>
2024-11-13 09:21:31 +0900

    safe
    주석

--- Assets/Script/SafeZone.cs
+++ Assets/Script/SafeZone.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.VisualScripting;
+//using UnityEngine;
 
-public class SafeZone : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+//public class SafeZone : MonoBehaviour
+//{
+//    void Start()
+//    {
         
-    }
+//    }
+//    void Update()
+//    {
+        
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//    void OnTriggerEnter2D(Collider2D other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+//            other.gameObject.tag = "none";
+//        }
+//    }
+//}
