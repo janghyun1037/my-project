c6068327b7d507d97965663e0a0ea67a65481cdc
janghyun1037 <hyunjhw@naver.com>
2024-11-21 08:17:34 +0900

    ?
    조금의 변경

--- Assets/Script/MoveCamera.cs
+++ Assets/Script/MoveCamera.cs
@@ -28,7 +28,7 @@ public class MoveCamera : MonoBehaviour
     {
         //transform.position = new Vector3(target.position.x, target.position.y + 3, -10f);     // 타겟 위치의 x값, 타겟 위치의 y값 이떄 y를 올릴려면 +를 하면됨
         transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
-        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);     //tlqkf 왜 y값이 ㅈㄴ 올라가는데
 
         float lx = size.x * 0.5f - width;
         float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
