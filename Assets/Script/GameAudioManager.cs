12bdfd84150fb5944250294daa8803a5c2fb9c42
janghyun1037 <hyunjhw@naver.com>
2024-12-01 21:03:41 +0900

    scenes
    각 씬별 bgm설정(야매)

--- Assets/Script/GameAudioManager.cs
+++ Assets/Script/GameAudioManager.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AudioManager : MonoBehaviour
+public class GameAudioManager : MonoBehaviour
 {
     [Header("-------------Audio Source------------")]
     [SerializeField] AudioSource musicSource;
 
     [Header("-------------Audio Clip------------")]
-    public AudioClip MainTHeme;     //솔직히 인게임이 제일 중요하긴해 근데 그렇다고 메인테마를 버리기엔 아까워.....
+    public AudioClip MainTheme;     //솔직히 인게임이 제일 중요하긴해 근데 그렇다고 메인테마를 버리기엔 아까워.....
     public AudioClip inGameTheme;   //선배한테 물어볼까? 이건 물어봐도 ㄱㅊ지 않을까? 쓰읍.......
 
     private void Start()
     {
-        musicSource.clip = MainTHeme;
+        musicSource.Stop();
+        musicSource.clip = inGameTheme;
         musicSource.Play();
     }
 }
