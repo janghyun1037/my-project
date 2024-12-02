9f979041bfa90069861d72f8b57f4b7539c4f39d
janghyun1037 <hyunjhw@naver.com>
2024-12-03 02:55:37 +0900

    the end
    엔딩

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("The END");
+        }
+    }
+}
